03fd28a619725320d26eb3f137a0fba2b2296e61
DaAlbrecht <dadidi42@gmail.com>
2023-08-21 20:24:01 +0200

    feat: lsp setup

---
+++ src/main.rs
@@ -0,0 +1,114 @@
+use ropey::Rope;
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                hover_provider: Some(HoverProviderCapability::Simple(true)),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: None,
+                    trigger_characters: Some(vec!["<".to_string()]),
+                    ..CompletionOptions::default()
+                }),
+
+                ..ServerCapabilities::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn hover(&self, params: HoverParams) -> Result<Option<Hover>> {
+        let contents = HoverContents::Markup(MarkupContent {
+            kind: MarkupKind::Markdown,
+            value: format!("Hello, world!\n\n```rust\n{:?}\n```", params),
+        });
+
+        let range = Some(Range {
+            start: Position {
+                line: 0,
+                character: 0,
+            },
+            end: Position {
+                line: 0,
+                character: 0,
+            },
+        });
+        Ok(Some(Hover { contents, range }))
+    }
+
+    async fn did_open(&self, _: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened!")
+            .await;
+    }
+
+    async fn did_change(&self, _: DidChangeTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file changed!")
+            .await;
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        let completion_list = CompletionList {
+            is_incomplete: false,
+            items: vec![
+                CompletionItem {
+                    label: "Hello, world!".to_string(),
+                    kind: Some(CompletionItemKind::TEXT),
+                    ..CompletionItem::default()
+                },
+                CompletionItem {
+                    label: "PayloadFactory".to_string(),
+                    kind: Some(CompletionItemKind::KEYWORD),
+                    detail: Some("PayloadFactory".to_string()),
+                    ..CompletionItem::default()
+                },
+            ],
+        };
+
+        Ok(Some(CompletionResponse::List(completion_list)))
+    }
+}
+struct TextDocumentItem {
+    uri: Url,
+    text: String,
+    version: i32,
+}
+
+impl Backend {
+    async fn on_change(&self, params: TextDocumentItem) {
+        let mut rope = Rope::from_str(&params.text);
+        //parse the text
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
